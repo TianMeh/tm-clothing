510895773fd0a484f1df471397da318b59b9ef4e
TianMeh <tikom99@gmail.com>
2023-01-25 22:22:05 +0100

    category-menu

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-
+import CategoryMenu from "./components/category-menu/category-menu.component";
 
 const App = () => {
-const categories = [
-    {
-      id: 1,
-      title: "hats",
-    },
-    {
-      id: 1,
-      title: "Jackets",
-    },
-    {
-      id: 1,
-      title: "Sneakers",
-    },
-    {
-      id: 1,
-      title: "Womens",
-    },
-    {
-      id: 1,
-      title: "Mens",
-    },
-  ];
+  
 
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div key={id} className="category-container">
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h1>{title}</h1>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <CategoryMenu />
   );
 };
 
